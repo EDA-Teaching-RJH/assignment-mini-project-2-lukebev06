63de9a076698c4f992d17292e0bcd67739019278
lukebev06 <lmb73@kent.ac.uk>
2026-03-12 01:52:17 +0000

    general outline of project

---
+++ main.py
@@ -0,0 +1,26 @@
+'''
+import for date/time
+import for csv
+import os
+
+make a csv dictionary with tuples. allows for 1 place for things to be changed within csv format
+
+function to read csv
+function to write csv
+function to update csv
+
+class hospital
+
+ class person
+
+  class patient
+  class staff
+  class doctor
+  class nurse
+  class receptionist
+
+ class appointment
+ class
+ 
+'''
+
